71be26786b32c27dd79ff70fd36259b26f592f50
Inan <khaninannyc@gmail.com>
2026-05-06 18:58:44 -0500

    Add assignment model

--- assignments/models.py
+++ assignments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from courses.models import Course
 
-# Create your models here.
+
+class Assignment(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    due_date = models.DateField(null=True, blank=True)
+    points_possible = models.IntegerField(default=100)
+
+    def __str__(self):
+        return self.title
