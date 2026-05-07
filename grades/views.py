b8fb3bfd7bd3edacf76db0827c680ab012bde341
mdespojo <mdespojo@uwm.edu>
2026-05-07 15:40:25 -0500

    started implementing grades for student

--- grades/views.py
+++ grades/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from accounts.models import StudentProfile
+from courses.models import Course
+
+
 # Create your views here.
+def grades(request):
+    user = request.user
+    if user.is_student():
+        # Retrieve all courses enrolled in
+        profile = StudentProfile.objects.get(user=user)
+        courses = Course.objects.filter(students=profile)
+    return render(request, 'viewgrades.html', {'courses': courses, 'student':profile.user.username})
--- learning_management_system/urls.py
+++ learning_management_system/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', include('core.urls')),
     path('accounts/', include('accounts.urls')),
     path('courses/', include('courses.urls')),
+    path('grades/', include('grades.urls')),
 ]
