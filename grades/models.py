ac2a8e21fb015b1f349ae271f718198afaf4a5c3
mdespojo <mdespojo@uwm.edu>
2026-05-09 23:08:42 -0500

    implemented grades for student

--- grades/models.py
+++ grades/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from assignments.models import Assignment
 
 # Create your models here.
+class AssignmentGrade(models.Model):
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    student = models.ForeignKey('accounts.StudentProfile', on_delete=models.CASCADE)
+    points_earned = models.IntegerField(default=0)
--- grades/views.py
+++ grades/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from accounts.models import StudentProfile
+from assignments.models import Assignment
 from courses.models import Course
+from grades.models import AssignmentGrade
 
 
 # Create your views here.
@@ -12,3 +14,28 @@ def grades(request):
         profile = StudentProfile.objects.get(user=user)
         courses = Course.objects.filter(students=profile)
     return render(request, 'viewgrades.html', {'courses': courses, 'student':profile.user.username})
+
+def course_grades(request, course_id):
+    user = request.user
+    if user.is_student():
+        profile = StudentProfile.objects.get(user=user)
+    course = Course.objects.get(id=course_id)
+    assignments = Assignment.objects.filter(course=course)
+    assignment_grades = []
+    for assignment in assignments:
+        grade = AssignmentGrade.objects.filter(assignment=assignment, student=profile).first()
+        assignment_grades.append({'assignment':assignment, 'grade':grade})
+    return render(request, 'view_course_grades.html', {'course': course, 'student': profile, 'assignment_grades': assignment_grades})
+
+def grade_assignment(request, assignment_id, student_id):
+    assignment = Assignment.objects.get(id=assignment_id)
+    student = StudentProfile.objects.get(id=student_id)
+    grade = AssignmentGrade.objects.get_or_create(assignment=assignment, student=student)
+
+    if request.method == 'POST':
+        points = request.post.get('points_earned')
+        grade.points_earned = points
+        grade.save()
+        return redirect('grades:course_grades', course_id=assignment.course.id)
+
+    return render(request, 'grade_assignment.html', {'grade': grade})
