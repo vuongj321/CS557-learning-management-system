6d7c783468044ff02f3213907e937dc17af9866b
vuongj321 <jasonvuong04@gmail.com>
2026-05-11 10:12:13 -0500

    Add assignment submit to urls

--- assignments/urls.py
+++ assignments/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.assignment_list, name='assignment_list'),
     path('create/', views.create_assignment, name='create_assignment'),
     path('course/<int:course_id>/', views.course_assignments, name='course_assignments'),
+    path('assignment/<int:assignment_id>/submit/', views.submit_assignment, name='submit_assignment'),
 ]
